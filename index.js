e98c927b074c0268569632c73637b40df22327b2
Lauri Laaksonen <lauri@laaksonen.io>
2016-04-24 23:13:27 +0300

    Change name of middleware folder

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 mongoose.connect(process.env.DB_CONNECTION);
 
 // App setup
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  next();
+});
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 routes(app);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const Authentication = require('../middleware/authentication');
+const Authentication = require('../controllers/authentication');
 const passport = require('passport');
 const passportService = require('../services/passport');
 
@@ -6,8 +6,8 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function (app) {
-  app.get('/', requireAuth, (req, res) => {
-    res.send({ hi: 'there' });
+  app.get('/secret', requireAuth, (req, res) => {
+    res.send({ message: 'The secret code is 93b2zk' });
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
